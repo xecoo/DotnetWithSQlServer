577492c29428ff3933467574737b4af9dbd1ff36
Bruno <bruno.pacheco@tusta.com.br>
2023-05-07 19:00:58 -0300

    feat: rename file product

--- src/Domain/Product/Product.cs
+++ src/Domain/Product/Product.cs
@@ -4,7 +4,7 @@ namespace Project.Domain.Product
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
