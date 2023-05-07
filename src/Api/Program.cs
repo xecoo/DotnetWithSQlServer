45e9aa57dc74e379d0ec92b35eea8cf454455067
Bruno <bruno.pacheco@tusta.com.br>
2023-05-07 17:55:20 -0300

    config: refact program file

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Project.Api
 {
-    public class Project
+    public class Program
     {
         public static void Main(string[] args)
         {            
