0fbc57d8c80e804e747b036801e4493d955950cb
Bruno <bruno.pacheco@tusta.com.br>
2023-05-07 14:07:22 -0300

    config: add swagger config on startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace Project.Api
 {
     public class Startup
@@ -12,12 +14,30 @@ namespace Project.Api
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddDbContext...
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(WebApplication app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
