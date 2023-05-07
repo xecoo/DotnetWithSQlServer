a51c6adb746c400c60b3d3e293b00da26a6345f0
Bruno <bruno.pacheco@tusta.com.br>
2023-05-07 17:55:11 -0300

    config: refact program file

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,26 +1,23 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 namespace Project.Api
 {
     public class Project
     {
         public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+        {            
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var createdDefaultBuilder = Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config.AddEnvironmentVariables(prefix: "Project_");
-            })
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
-
-            return createdDefaultBuilder;
-        }
     }
 }
 
