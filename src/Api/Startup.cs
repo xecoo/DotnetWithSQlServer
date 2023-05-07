dc56b22681dae9d1478d1047a8a085f9218b7408
Bruno <bruno.pacheco@tusta.com.br>
2023-05-07 17:55:53 -0300

    config: config startup file adding IApplicationBuilder

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
 namespace Project.Api
@@ -5,6 +9,7 @@ namespace Project.Api
     public class Startup
     {
         public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
@@ -13,31 +18,30 @@ namespace Project.Api
         // This method gets called by the runtime. Use this method to add serices to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext...
             services.AddControllers();
+            
+            services.AddEndpointsApiExplorer();
+            
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
             });
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(WebApplication app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
             app.UseSwagger();
             app.UseSwaggerUI(
                 x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
             app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
