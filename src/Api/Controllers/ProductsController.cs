8c89be2e0a376e43ea53d1f70d7ab74268fc3db5
Bruno <bruno.pacheco@tusta.com.br>
2023-05-08 00:51:45 -0300

    feat: create route to register product

--- src/Api/Controllers/ProductsController.cs
+++ src/Api/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Project.Api.Controllers.Resources;
 using Project.Application.UseCases.GetProducts;
+using Project.Application.UseCases.RegisterProduct;
 
 namespace Project.Api.Controllers
 {
@@ -15,6 +17,19 @@ namespace Project.Api.Controllers
             [FromServices] IMediator mediator)
         {
             var request = new GetProductsRequest();
+            
+            var response = await mediator.Send(request);
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Product")]
+        public async Task<IActionResult> RegisterProduct(
+            [FromBody] RegisterProductDto productDto,
+            [FromServices] IMediator mediator)
+        {
+            var request = new RegisterProductRequest(productDto.Name, productDto.Description);
             
             var response = await mediator.Send(request);
 
