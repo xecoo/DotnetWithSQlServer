3d3e12afb24f6f6c64b07ee54d234111ffeb6845
Bruno <bruno.pacheco@tusta.com.br>
2023-05-08 00:52:02 -0300

    feat: create resource

---
+++ src/Api/Controllers/Resources/RegisterProductDto.cs
@@ -0,0 +1,8 @@
+namespace Project.Api.Controllers.Resources
+{
+    public class RegisterProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
